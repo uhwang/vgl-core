00246b86a402584289278cfe9e7fbbaf1aa88cb3
uhwang <43251090+uhwang@users.noreply.github.com>
2023-08-19 19:58:01 -0400

    Bug fix

--- libvgl/vgl/vertex.py
+++ libvgl/vgl/vertex.py
@@ -13,11 +13,16 @@ import numpy as np
 from . import util
 
 class Vertex():
-    def __init__(self, nvert, dim=2):
+    def __init__(self, nvert, dim=2, src=None):
         self.nvert = nvert
         self.dim = dim
         self.vertex = np.empty(nvert*dim, dtype='float32')
         
+        if isinstance(src, np.ndarray) or\
+           isinstance(src, list) or\
+           isinstance(src, tuple):
+            self.point = src
+            
     def get_vertices(self): return self.vertex
     def get_vertex(self, index): return [self.vertex[index*2], self.vertex[index*2+1]]
     def get_nvertex(self): return int(self.vertex.size/2)
@@ -31,6 +36,10 @@ class Vertex():
         rm = util.deg_rot_matrix(deg)
         self.vertex = np.array([ np.dot(rm, v) 
                                  for v in self.vertex.reshape(self.nvert,2) ]).flatten()
+        return self
+        
+    def npoint(self):
+        return self.nvert
         
     def transx(self, tx):
         self.vertex[ ::2] += tx
@@ -41,9 +50,51 @@ class Vertex():
     def trans(self, tx, ty):
         self.transx(tx)
         self.transy(ty)
+        return self
+    
+    def join(self, src):
+        vv = np.concatenate((self.vertex, src.vertex), axis=None)
+        return Vertex(vv.size//self.dim, src=vv)
         
-#class Vertex3d(Vertex):
-#	def __init__(self, nvert):
-#		super.__init__(nvert, dim=3)
-		
+    def endpoint(self):
+        return self.vertex[-2], self.vertex[-1]
+        
+    def firstpoint(self):
+        return self.vertex[0], self.vertex[1]
+                
+    def xmin(self):
+        return self.vertex[::2].min()
+
+    def xmax(self):
+        return self.vertex[::2].max()
+        
+    def ymin(self):
+        return self.vertex[1::2].min()
+        
+    def ymax(self):
+        return self.vertex[1::2].max()
+        
+    @property
+    def point(self):
+        return self.vertex
+        
+    # alist should be one dim array, list, or tuple
+    @point.setter
+    def point(self, alike):
+        if hasattr(alike, "__iter__"):
+            if isinstance(alike, np.ndarray): 
+                size = alike.size
+            if isinstance(alike, list) or\
+               isinstance(alike, tuple):
+                size = len(alike)
+               
+            if size == self.vertex.size:
+                for i, x in enumerate(alike):
+                    self.vertex[i] = x
+            else:
+                raise ValueError('Vertes: dimension mismatch')
+                
+    # iterable : array, tuple, list
+    def copy(self):
+        return Vertex(self.nvert, src=self.vertex)
 
