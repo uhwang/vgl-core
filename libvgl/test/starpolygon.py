0b071f230d1d36ec29c816388b2b2c9b5654cf31
uhwang <43251090+uhwang@users.noreply.github.com>
2023-12-16 00:47:14 -0500

    Add files via upload

--- libvgl/test/starpolygon.py
+++ libvgl/test/starpolygon.py
@@ -4,8 +4,6 @@ import libvgl as vgl
 r = 1.0
 fmm = vgl.FrameManager()
 frm = fmm.create(0,0,4,4, vgl.Data(-r,r,-r,r))
-dev = vgl.DeviceIMG("./starpolygon/star.jpg", fmm.get_gbbox(), 300)
-dev.set_device(frm)
 star = vgl.basicshape.StarPolygon(0,0,5)
 uu, max_u = 1, r/star.u_radius
 fps, duration= 10, 20
@@ -14,10 +12,10 @@ du = max_u/total_frame
 f0 = star.u_radius/du
 
 def draw(t):
-    global dev, uu
-    dev.fill_white()
-    vgl.draw_center_axis(dev)
-    dev.polygon(star.xss, star.yss, vgl.color.BLUE, 0.005)
+    global dev_img, uu
+    dev_img.fill_white()
+    vgl.draw_center_axis(dev_img)
+    dev_img.polygon(star.xss, star.yss, vgl.color.BLUE, 0.005)
 
     if t*fps < f0:
         uu -= du
@@ -28,8 +26,9 @@ def draw(t):
 
     star.u_param = uu
 
-def plot():
-    dev.fill_white()
+def plot(dev):
+    dev.set_device(frm)
+
     uu = np.linspace(0.2, 1.3, 10)
     for u in uu:
         star.u_param = u
@@ -38,16 +37,31 @@ def plot():
     
 def save():
     from . import chkfld
-    global dev
+    global dev_img
     
     if not chkfld.create_folder("./starpolygon"):
         return
-
-    dev_mov = vgl.DeviceCairoAnimation("./starpolygon/starpolygon.mp4", dev, draw, fps=fps, 
+    
+    gbox = fmm.get_gbbox()
+    dev_img = vgl.DeviceIMG("./starpolygon/star.jpg", gbox, 300)
+    dev_pdf = vgl.DevicePDF("./starpolygon/star.pdf", gbox)
+    dev_wmf = vgl.DeviceWMF("./starpolygon/star.wmf", gbox)
+    dev_emf = vgl.DeviceEMF("./starpolygon/star.emf", gbox)
+    dev_svg = vgl.DeviceSVG("./starpolygon/star.svg", gbox, 300)
+    dev_ppt = vgl.DevicePPT("./starpolygon/star.pptx",gbox)
+    
+    dev_img.set_device(frm)
+    dev_mov = vgl.DeviceCairoAnimation("./starpolygon/starpolygon.mp4", dev_img, draw, fps=fps, 
                                        duration=duration)
     dev_mov.save_video()
 
-    plot()
+    dev_img.fill_white()
+    plot(dev_img)
+    plot(dev_pdf)
+    plot(dev_wmf)
+    plot(dev_emf)
+    plot(dev_svg)
+    plot(dev_ppt)
     
 if __name__ == "__main__":
     save()
