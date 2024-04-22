ba2ad80dae040ef30d8d8349152fdb405c09e0f6
John <43251090+uhwang@users.noreply.github.com>
2024-04-21 21:09:09 -0400

    Fix arg order

--- libvgl/vgl/arc.py
+++ libvgl/vgl/arc.py
@@ -1,5 +1,7 @@
 '''
-ellipse.py
+arc.py
+
+4/20/2024
 
 '''
 import numpy as np
@@ -9,23 +11,22 @@ from . import color
 from . import linepat
 
 def _arc(dev,
-            xc           ,  # the x coord of the center
-            yc           ,  # the y coord of the center
-            radius       ,  # radius of the Arc
-            start_angle  ,  # (degree)
-            end_angle    ,  # (degree)
-            line_in      ,  # Draw line from center to start/end pnt (T/F)
-            line_out     ,  # Draw perimeter (T/F)
-            lcol_in      ,  # line color
-            lthk_in      ,  # line thickness
-            lcol_out     ,  # line color
-            lthk_out     ,  # line thickness
-            lpat_in      , 
-            lpat_out     , 
-            fcol         , 
-            fill         , 
-            viewport     ,
-            ):
+         xc           ,  # the x coord of the center
+         yc           ,  # the y coord of the center
+         radius       ,  # radius of the Arc
+         start_angle  ,  # (degree)
+         end_angle    ,  # (degree)
+         line_in      ,  # Draw line from center to start/end pnt (T/F)
+         lcol_in      ,  # line color
+         lthk_in      ,  # line thickness
+         lpat_in      ,  # line pattern
+         line_out     ,  # Draw perimeter (T/F)
+         lcol_out     ,  # line color
+         lthk_out     ,  # line thickness
+         lpat_out     ,  # line pattern perimeter
+         fcol         ,  # fill color (Color obj or None:default)
+         viewport     ,
+         ):
     
     ang1_rad = util.deg_to_rad(start_angle)
     ang2_rad = util.deg_to_rad(end_angle)
@@ -34,7 +35,7 @@ def _arc(dev,
     xx = xc + radius*np.cos(ang3) 
     yy = yc + radius*np.sin(ang3) 
     
-    if fill:
+    if isinstance(fcol, color.Color):
         if viewport:
             dev.lpolygon(xx,yy,lcol=None,fcol=fcol)
         else:
@@ -54,80 +55,75 @@ def _arc(dev,
         else:
             dev.polyline(xx,yy,lcol_out,lthk_out,lpat_out)
         
-    
 def arc(dev,
-            xc            = 0,            # the x coord of the center
-            yc            = 0,            # the y coord of the center
-            radius        = 1,            # radius of the Arc
-            start_angle   = 0,            # (degree)
-            end_angle     = 45,           # (degree)
-            line_in       = False,         # Draw line from center to start/end pnt (T/F)
-            line_out      = True,        # Draw perimeter (T/F)
-            lcol_in       = color.BLACK , # line color
-            lthk_in       = 0.001,        # line thickness
-            lcol_out      = color.BLACK , # line color
-            lthk_out      = 0.001,        # line thickness
-            lpat_in       = linepat._PAT_SOLID, # line pattern from center to start & end point
-            lpat_out      = linepat._PAT_SOLID, # line pattern perimeter
-                                                # _PAT_SOLID is string, others are class obj
-            fcol          = color.beige,  # fill color
-            fill          = False,        # T/F
-            ):
-            
+        xc            = 0,            # the x coord of the center
+        yc            = 0,            # the y coord of the center
+        radius        = 1,            # radius of the Arc
+        start_angle   = 0,            # (degree)
+        end_angle     = 45,           # (degree)
+        line_in       = False,        # Draw line from center to start/end pnt (T/F)
+        lcol_in       = color.BLACK , # line color
+        lthk_in       = 0.001,        # line thickness
+        lpat_in       = linepat._PAT_SOLID, # line pattern ()
+                                            # _PAT_SOLID is string, others are class obj
+        line_out      = True,         # Draw perimeter (T/F)
+        lcol_out      = color.BLACK , # line color
+        lthk_out      = 0.001,        # line thickness
+        lpat_out      = linepat._PAT_SOLID, # line pattern perimeter
+                                            # _PAT_SOLID is string, others are class obj
+        fcol          = None          # fill color
+        ):
             
     _arc(
-            dev,
-            xc,
-            yc,
-            radius,
-            start_angle,
-            end_angle,
-            line_in,
-            line_out,
-            lcol_in,
-            lthk_in,
-            lcol_out,
-            lthk_out,
-            lpat_in,
-            lpat_out,
-            fcol,
-            fill,
-            viewport = False)
+        dev,
+        xc,
+        yc,
+        radius,
+        start_angle,
+        end_angle,
+        line_in,
+        lcol_in,
+        lthk_in,
+        lpat_in,
+        line_out,
+        lcol_out,
+        lthk_out,
+        lpat_out,
+        fcol,
+        viewport = False)
             
-def larc(dev,
-            xc            = 0,            # the x coord of the center
-            yc            = 0,            # the y coord of the center
-            radius        = 1,            # radius of the Arc
-            start_angle   = 0,            # (degree)
-            end_angle     = 45,           # (degree)
-            line_in       = False,         # Draw line from center to start/end pnt (T/F)
-            line_out      = True,        # Draw perimeter (T/F)
-            lcol_in       = color.BLACK , # line color
-            lthk_in       = 0.001,        # line thickness
-            lcol_out      = color.BLACK , # line color
-            lthk_out      = 0.001,        # line thickness
-            lpat_in       = linepat._PAT_SOLID, # line pattern from center to start & end point
-            lpat_out      = linepat._PAT_SOLID, # line pattern perimeter
-                                                # _PAT_SOLID is string, others are class obj
-            fcol          = color.beige,  # fill color
-            fill          = False,        # T/F
-            ):
-            
+def arc(dev,
+        xc            = 0,            # the x coord of the center
+        yc            = 0,            # the y coord of the center
+        radius        = 1,            # radius of the Arc
+        start_angle   = 0,            # (degree)
+        end_angle     = 45,           # (degree)
+        line_in       = False,        # Draw line from center to start/end pnt (T/F)
+        lcol_in       = color.BLACK , # line color
+        lthk_in       = 0.001,        # line thickness
+        lpat_in       = linepat._PAT_SOLID, # line pattern ()
+                                            # _PAT_SOLID is string, others are class obj
+        line_out      = True,         # Draw perimeter (T/F)
+        lcol_out      = color.BLACK , # line color
+        lthk_out      = 0.001,        # line thickness
+        lpat_out      = linepat._PAT_SOLID, # line pattern perimeter
+                                            # _PAT_SOLID is string, others are class obj
+        fcol          = None          # fill color
+        ):
             
     _arc(dev,
-            xc,
-            yc,
-            radius,
-            start_angle,
-            end_angle,
-            line_in,
-            line_out,
-            lcol_in,
-            lthk_in,
-            lcol_out,
-            lthk_out,
-            lpat_in,
-            lpat_out,
-            fcol,
-            fill,
-            viewport = True)            
+         xc,
+         yc,
+         radius,
+         start_angle,
+         end_angle,
+         line_in,
+         line_out,
+         lcol_in,
+         lthk_in,
+         lcol_out,
+         lthk_out,
+         lpat_in,
+         lpat_out,
+         fcol,
+         viewport = True)            
